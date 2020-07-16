1e8379bce492d3d4575800632f77852354d900df
timilsina-devi <49350389+timilsina-devi@users.noreply.github.com>
2020-07-16 01:30:32 -0230

    Updates

---
+++ precache-manifest.40003ffa78ed1d8235e7ee90f1753e31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c23466f352a4a891cf1a63a6da91f88",
+    "url": "/devi-portfolio/index.html"
+  },
+  {
+    "revision": "370fbf713603be9c5b59",
+    "url": "/devi-portfolio/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "0eec68536f9ef71d9dec",
+    "url": "/devi-portfolio/static/css/main.77bc1c26.chunk.css"
+  },
+  {
+    "revision": "370fbf713603be9c5b59",
+    "url": "/devi-portfolio/static/js/2.eee39aa9.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/devi-portfolio/static/js/2.eee39aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eec68536f9ef71d9dec",
+    "url": "/devi-portfolio/static/js/main.3dc32ec1.chunk.js"
+  },
+  {
+    "revision": "41e81dcc292b1c672eed",
+    "url": "/devi-portfolio/static/js/runtime-main.02a64165.js"
+  },
+  {
+    "revision": "876996a0e568ed1fe011446faf1e873c",
+    "url": "/devi-portfolio/static/media/devi.876996a0.jpg"
+  }
+]);
